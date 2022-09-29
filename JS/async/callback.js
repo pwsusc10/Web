61a77200bf9e68d3c1d07b6c624060e3be1b29c2
Sungpyo <pwsusc10@naver.com>
2022-09-29 17:10:15 +0900

    add callback grammer

---
+++ JS/async/callback.js
@@ -0,0 +1,83 @@
+// 'use strict';
+
+// // Javascript is synchronous
+// // Execute the code block in order after hoisting.
+// // hoisting: var, function declaration들이 제일 위로 올라가는 것
+
+// console.log('1');
+// // callback function: 우리가 전달해준 함수를 나중에 너가 불러줘!
+// // setTimeout(function() {
+// //     console.log('2');
+// // }, 1000);
+// setTimeout(() => console.log('2'), 1000);
+// console.log('3');
+
+
+// // Synchronous callback
+// function printImmediately(print){
+//     print();
+// };
+
+// printImmediately(()=> console.log('hello'));
+
+// // Asynchronous callback
+// function printWithDelay(print, timeout) {
+//     setTimeout(print, timeout);
+// };
+// printWithDelay(() => console.log('async callback'), 2000);
+
+
+// // Callback Hell examle
+// class UserStorage {
+//     loginUser(id, password, onSuccess, onError) {
+//         setTimeout(() =>{
+//             if(
+//                 (id === 'sungpyo' && password === '1234') ||
+//                 (id === 'ajou' && password === 'univer')
+//             ) {
+//                 onSuccess(id);
+//             } else {
+//                 onError(new Error('not found'));
+//             }
+//         }, 2000);
+//     } 
+
+//     getRoles(user, onSuccess, onError) {
+//         setTimeout(() =>{
+//             if(user === 'sungpyo') {
+//                 onSuccess({ name: 'sungpyo', role: 'admin'});
+//             } else {
+//                 onError(new Error('no access'));
+//             }
+//         }, 1000);
+//     }
+// }
+
+// const userStorage= new UserStorage();
+// const id= prompt("enter your id");
+// const passwd= prompt('enter your password');
+// // userStorage.loginUser(id,)
+// userStorage.loginUser(
+//     id,
+//     passwd,
+//     user => {
+//         userStorage.getRoles(
+//             user,
+//             userWithRole=> {
+//                 alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+//             },
+//             error => {
+//                 console.log(error);
+//             }
+//         );
+//     },
+//     error => {console.log(error)}
+// );
+
+// 함수로 호출해도...
+const x = Error('함수 호출로 만들기!')
+console.log(x);
+
+// ...이렇게 생성자로 사용한 것과 같은 결과
+const y = new Error('"new" 키워드를 써서 만들기!')
+console.log(y);
